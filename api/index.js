6a083b245458ae491d303cfa47f56074fe90ffe7
prikol769 <prikol769@gmail.com>
2024-04-26 12:35:57 -0500

    added mongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log("MongoDb is connected!"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
