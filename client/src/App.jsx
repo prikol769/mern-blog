555ca551b4a1729c080263c7e7db80172812235e
prikol769 <prikol769@gmail.com>
2024-04-27 21:26:07 -0500

    added sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+import SingIn from "./pages/SingIn";
+import SingUp from "./pages/SingUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
@@ -14,8 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sing-in" element={<SingIn />} />
+        <Route path="/sing-up" element={<SingUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
