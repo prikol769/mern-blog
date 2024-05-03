445d915f4106a77eba91e2283ae18dcef91443a1
prikol769 <prikol769@gmail.com>
2024-05-03 12:38:45 -0500

    added update post functionality

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,5 +7,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin(), require("tailwind-scrollbar")],
+  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
 };
