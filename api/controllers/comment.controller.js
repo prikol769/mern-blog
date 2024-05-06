23582467ec4cd78bd3b4b9b35b228a231d005116
prikol769 <prikol769@gmail.com>
2024-05-06 12:42:28 -0500

    added getPostComments route

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,6 +1,17 @@
 import Comment from "../models/comment.model.js";
 import { errorHandler } from "../utils/error.js";
 
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const createComment = async (req, res, next) => {
   try {
     const { content, userId, postId } = req.body;
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  getPostComments,
+} from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
+router.get("/getPostComments/:postId", getPostComments);
 router.post("/create", verifyToken, createComment);
 
 export default router;
