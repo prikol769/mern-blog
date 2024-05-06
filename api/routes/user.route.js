40b92c1fe46378cfc5a829180a305f656c89b332
prikol769 <prikol769@gmail.com>
2024-05-06 12:55:59 -0500

    added getUser route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getusers,
   signout,
   test,
@@ -15,5 +16,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getusers);
+router.get("/:userId", getUser);
 
 export default router;
