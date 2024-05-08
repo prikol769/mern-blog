2362c183ca11a6dab382414e1f2ecbdab71a882a
prikol769 <prikol769@gmail.com>
2024-05-07 19:27:55 -0500

    added likeComment route

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -3,11 +3,13 @@ import { verifyToken } from "../utils/verifyUser.js";
 import {
   createComment,
   getPostComments,
+  likeComment,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.get("/getPostComments/:postId", getPostComments);
 router.post("/create", verifyToken, createComment);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
 
 export default router;
