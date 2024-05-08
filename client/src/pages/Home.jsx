64177ce53a56b02a91c3f331cc2cdf3405c16792
prikol769 <prikol769@gmail.com>
2024-05-08 12:55:06 -0500

    fix home page PostCard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-7xl mx-auto ">
         <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
         <p className="text-gray-500 text-xs sm:text-sm">
           Here you'll find a variety of articles and tutorials on topics such as
@@ -33,11 +33,11 @@ export default function Home() {
         <CallToAction />
       </div>
 
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+      <div className="lg:max-w-7xl mx-auto p-3 flex flex-col gap-8 py-7">
         {posts && posts.length > 0 && (
           <div className="flex flex-col gap-6">
             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-            <div className="flex flex-wrap gap-4">
+            <div className="flex flex-wrap gap-4 justify-center xl:justify-start">
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
