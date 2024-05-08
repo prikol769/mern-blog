14a824ed0b39d5200a5baee486797111c8e28aaf
prikol769 <prikol769@gmail.com>
2024-05-08 13:03:03 -0500

    setup up __dirname

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
   .then(() => console.log("MongoDb is connected!"))
   .catch((err) => console.log(err));
+
+const __dirname = path.resolve();
 
 const app = express();
 
