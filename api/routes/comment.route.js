3393283dbdaa9c2ca1d18781c511dd6b8f33af4b
prikol769 <prikol769@gmail.com>
2024-05-07 19:44:49 -0500

    added editComment route

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -4,6 +4,7 @@ import {
   createComment,
   getPostComments,
   likeComment,
+  editComment,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/getPostComments/:postId", getPostComments);
 router.post("/create", verifyToken, createComment);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 
 export default router;
